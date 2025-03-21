efa7e3c1d42b64f4abe707bd8de087e1a8874857
mkay <mkay@gmail.com>
2025-03-21 22:40:33 +0300

    second commit mahn

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const change = document.getElementById("okay").innerHTML="WE ARE BAD"
-console.log(change)
-
-
-
-function demo() {
-    document.getElementById("demo").innerHTML="not so good"
-}
-console.log(demo)
-
-
-const demo1 = document.getElementById("demo").innerText="very okay"
-console.log(demo1)
-
-
 // Use innerHTML when you want to change an HTML element.
+   const change = document.getElementById("okay").innerHTML="WE ARE BAD"
+   console.log(change)
+
+  
+
+   function demo() {
+      document.getElementById("demo").innerHTML="not so good"
+   }
+   console.log(demo)
+
 
 // Use innerText when you only want to change the plain text.
-    
+   const demo1 = document.getElementById("demo").innerText="very okay"
+   console.log(demo1)
+ 
+
+// You can use an alert box to display data:
+    window.alert("ola mucho")
+
+
