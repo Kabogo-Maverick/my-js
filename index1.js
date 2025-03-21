d56d07e6b0bceba2d710e8f86199fe61d8a724f1
mkay <mkay@gmail.com>
2025-03-22 00:16:25 +0300

    third commit mahn

---
+++ index1.js
@@ -0,0 +1,13 @@
+let last1 , last2;
+last1="ken"
+last2="bob"
+console.log(last1,last2)
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total)
+
+let fruits = ["apple", "oranges", "melon"]
+document.getElementById("ken").innerHTML = fruits
+console.log(fruits)
