5176462fc4bf0f1e47b201f6215fdd5ed5c0831f
mkay <mkay@gmail.com>
2025-03-21 22:10:22 +0300

    firt commit mahn

---
+++ index.js
@@ -0,0 +1,19 @@
+const change = document.getElementById("okay").innerHTML="WE ARE BAD"
+console.log(change)
+
+
+
+function demo() {
+    document.getElementById("demo").innerHTML="not so good"
+}
+console.log(demo)
+
+
+const demo1 = document.getElementById("demo").innerText="very okay"
+console.log(demo1)
+
+
+// Use innerHTML when you want to change an HTML element.
+
+// Use innerText when you only want to change the plain text.
+    
