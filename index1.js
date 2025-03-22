b8b3e20a2568601163281bba714a262f319a2a3a
mkay <mkay@gmail.com>
2025-03-22 15:21:10 +0300

    fourth commit mahn

--- index1.js
+++ index1.js
@@ -8,6 +8,15 @@ const price2 = 6;
 let total = price1 + price2;
 console.log(total)
 
-let fruits = ["apple", "oranges", "melon"]
-document.getElementById("ken").innerHTML = fruits
-console.log(fruits)
+// let fruits = ["apple", "oranges", "melon"]
+// document.g  etElementById("ken").innerHTML = fruits
+// console.log(fruits)
+
+let a = 20
+let x = (200-100)*a
+console.log(x)
+
+let p = 30
+let q = 40
+let r = 30
+console.log(q==r)
